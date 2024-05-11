c7ea2b0e2b38efcae4cbe2b8450395803213d42e
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-05-11 23:48:26 +0500

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+let date = new Date();
+let year = date.getFullYear();
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>Copyright © {year} - All right reserved by Davronbek Anvarov</p>
+      </aside>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
